dbd2390e367e7faccb7a826e308079645852d32c
UnleachedMe <ayman.nabih1993@gmail.com>
2025-07-07 00:28:53 +0100

    Successfuly updated the UI and linked with backend project

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,12 +1,19 @@
 package net.kzn.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+import net.kzn.shoppingbackend.dto.Category;
+
 @Controller
 public class PageController {
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
 	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
@@ -14,6 +21,9 @@ public class PageController {
 		//mv.addObject("greeting", "Hello fucking world!");
 		mv.addObject("title", "Home");
 		mv.addObject("userClickHome", true);
+		
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
 		
 		return mv;
 	}
@@ -35,6 +45,36 @@ public class PageController {
 		mv.addObject("title", "Contact Us");
 		mv.addObject("userClickContact", true);
 		
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/show/all/products"})
+	public ModelAndView showAllProducts() {
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Hello fucking world!");
+		mv.addObject("title", "All Products");
+		mv.addObject("userClickAllProducts", true);
+		
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/show/category/{id}/products"})
+	public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Hello fucking world!");
+		
+		Category category = null;
+		category = categoryDAO.get(id);
+		
+		mv.addObject("title", category.getName());
+		mv.addObject("userClickCategoryProducts", true);
+		
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		mv.addObject("category", category);
 		return mv;
 	}
 	
